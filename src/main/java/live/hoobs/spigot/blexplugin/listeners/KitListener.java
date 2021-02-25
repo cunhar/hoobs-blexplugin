a16d57d25cd6b1e59e1070a95fe4ec89c1798f4f
RealBaconEater <cunhar@gmail.com>
2021-02-25 19:45:00 +0000

    Make it more efficient and add acutal points

--- src/main/java/live/hoobs/spigot/blexplugin/listeners/KitListener.java
+++ src/main/java/live/hoobs/spigot/blexplugin/listeners/KitListener.java
@@ -5,6 +5,7 @@ import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Color;
+import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -19,9 +20,9 @@ import org.bukkit.inventory.meta.LeatherArmorMeta;
 public class KitListener implements Listener {
     final World world = Bukkit.getServer().getWorld("world");
 
-    final Location resetLocation = new Location( world, 159, 58, 82);
+    final Location resetLocation = new Location( world, 176, 68, 82);
 
-    final Location armorLocation = new Location( world, 145, 57, 81);     
+    final Location armorLocation = new Location( world, 178, 36, 55);     
     final ItemStack[] armorItems = {
         new ItemStack(Material.LEATHER_HELMET),
         new ItemStack(Material.LEATHER_CHESTPLATE),
@@ -29,13 +30,13 @@ public class KitListener implements Listener {
         new ItemStack(Material.LEATHER_BOOTS)
     };
 
-    final Location claimLocation = new Location( world, 145, 47, 81);     
+    final Location claimLocation = new Location( world, 157, 23, 77);     
     final ItemStack[] claimItems = {
         new ItemStack(Material.STICK),
         new ItemStack(Material.GOLDEN_SHOVEL)
     };
     
-    final Location toolsLocation = new Location( world, 145, 37, 81);   
+    final Location toolsLocation = new Location( world, 162, 8, 44);   
     final ItemStack[] toolsItems = {
         new ItemStack(Material.WOODEN_AXE),
         new ItemStack(Material.WOODEN_HOE),
@@ -44,17 +45,17 @@ public class KitListener implements Listener {
         new ItemStack(Material.WOODEN_SWORD)
     };
 
-    final Location foodLocation = new Location( world, 145, 27, 81);   
+    final Location foodLocation = new Location( world, 171, 3, 54);   
     final ItemStack[] foodItems = {
         new ItemStack(Material.BAKED_POTATO, 16)
     };
 
-    final Location torchesLocation = new Location( world, 145, 17, 81);   
+    final Location torchesLocation = new Location( world, 172, 28, 76);   
     final ItemStack[] torchesItems = {
         new ItemStack(Material.TORCH, 16)
     };
 
-    final Location saplingsLocation = new Location( world, 145, 7, 81);   
+    final Location saplingsLocation = new Location( world, 150, 49, 58);   
     final ItemStack[] saplingsItems = {
         new ItemStack(Material.OAK_SAPLING, 16)
     };
@@ -69,8 +70,32 @@ public class KitListener implements Listener {
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent event) {
+        final Location fromLocation = event.getFrom();
         final Location location = event.getTo();
-        final Player player = event.getPlayer(); 
+        final Player player = event.getPlayer();
+        
+        //only survival
+        if(player.getGameMode() != GameMode.SURVIVAL){
+            return;
+        }
+        
+        final int intX = (int)fromLocation.getX();
+        final int intY = (int)fromLocation.getY();
+        final int intZ = (int)fromLocation.getZ();
+        final int intToX = (int)location.getX();
+        final int intToY = (int)location.getY();
+        final int intToZ = (int)location.getZ();
+
+        // not enough
+        if (intX == intToX && intY == intToY && intZ == intToZ){
+            return;
+        }
+
+        //nowhere near
+        if (intX > 179 || intToX < 149 || intY > 69 || intToY < 2 || intZ > 83 || intToZ < 43) {
+            return;
+        }
+        
         final Set<String> tags = player.getScoreboardTags();
 
         // armor
@@ -96,14 +121,14 @@ public class KitListener implements Listener {
     }
 
     public void checkKit(Player player, Location playerLocation, Location location, String tag, Set<String> tags, ItemStack[] items){
-        if (playerLocation.distance(location) < 2 && !tags.contains(tag)) {
+        if (playerLocation.distanceSquared(location) < 2 && !tags.contains(tag)) {
             player.addScoreboardTag(tag);
             player.getInventory().addItem(items);
         }
     }
 
     public void resetKit(Player player, Location playerLocation, Location location, String tagSeed, Set<String> tags ) {
-        if (tags != null && playerLocation.distance(location) < 2){
+        if (tags != null && playerLocation.distanceSquared(location) < 2){
             tags.removeIf(tag -> (tag.startsWith(tagSeed)));
         }
     }
